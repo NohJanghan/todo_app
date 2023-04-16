656c39dddb9cba1b9ce4fe12952e2257a238fac0
Janghan Noh <felkared@gmail.com>
2023-04-16 19:31:29 +0900

    Fix window sizing and Add .DS_Store in .gitignore

--- main.js
+++ main.js
@@ -3,7 +3,8 @@ const { app, BrowserWindow } = require('electron')
 const createWindow = () => {
     const win = new BrowserWindow({
         width: 800,
-        height: 600
+        height: 600,
+        resizable: false
     })
 
     win.loadFile('main.html')

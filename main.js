b98234ca4e6eb10710bd66a214ec278a7c9a2f94
Janghan Noh <felkared@gmail.com>
2023-04-28 00:49:11 +0900

    Set ENV

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 const { app, BrowserWindow } = require('electron')
+require("dotenv").config();
+
 
 const createWindow = () => {
     const win = new BrowserWindow({
@@ -8,7 +10,7 @@ const createWindow = () => {
     })
 
     win.loadFile('main.html')
-    win.webContents.openDevTools()
+    if(process.env.NODE_ENV == "development") win.webContents.openDevTools()
 }
 
 app.whenReady().then(() => {
